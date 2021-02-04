f3aba6cd60e2ee05aa0eba3bcf54dec3556df9f6
Tinkergit <nicholas.stagno@gmail.com>
2021-02-04 17:14:51 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,28 @@
+def my_collect(collection)
+    output_array = []
+
+    counter = 0
+
+    while counter < collection.length
+
+       
+        output_array << yield(collection[counter])
+        counter += 1
+        
+
+    end
+
+    output_array
+end
+
+
+
+
+
+    
+
+
+
+
 
 
